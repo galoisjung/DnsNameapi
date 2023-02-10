42d40bd85a884541dfd317a01055a0348085381e
juho jung <jhjung@t3q.com>
2023-02-10 15:48:04 +0900

    idea 삭

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 pw=`dirname $0`
-#export PATH=$pw:$PATH
-#export OPENSSL_CONF=$pw/openssl.cnf
+export PATH=$pw:$PATH
+export OPENSSL_CONF=$pw/openssl.cnf
 python3 $pw/main.py
 today=$(date)
 echo "-------" $today " End of Process-------"
